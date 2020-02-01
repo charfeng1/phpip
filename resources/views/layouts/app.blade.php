charfeng1/phpip

commit 41197a2ff6009ceb3d441ec004870aade5470354

Replaced "select" fields again by autocomplete fields with 0 min length. Can't get them to trigger on focus or click, a key needs to be pressed, be it "space" to display all entries

resources/views/layouts/app.blade.php
@@ -300,7 +300,8 @@
               });
           }
           break;
-    }
+      }
+
       /* Various functions used here and there */
 
       // Nationalize modal
@@ -404,19 +405,28 @@
       if (e.target.matches(".noformat, textarea, [contenteditable]")) {
         e.target.classList.add("border", "border-info");
       }
+    });
 
-      // ui-front class required for showing selection list with jQuery autocomplete in modals
-      if ( e.target.closest('.modal-content') ) {
+    app.addEventListener("focusin", e => {
+      // Process autocomplete fields
+      if ( e.target.hasAttribute('data-ac') ) {
+        var aclength = 2;
+        if ( e.target.hasAttribute('data-aclength') ) {
+          aclength = e.target.dataset.aclength;
+        }
         if (e.target.closest('tr')) {
           e.target.closest('tr').classList.add('ui-front');
         }
-      }
-
-      // Process autocomplete fields
-      if ( e.target.hasAttribute('data-ac') ) {
         $(e.target).autocomplete({
           autoFocus: true,
+          minLength: aclength,
           source: e.target.dataset.ac,
+          create: function(event, ui) {
+            // Fires search immediately (but selection does not trigger blur or change)
+            // if ( aclength == 0 ) {
+            //   $(this).autocomplete("search", "");
+            // }
+          },
           select: (event, ui) => {
             if (e.target.id == 'addCountry') {
               let newCountry = appendCountryTemplate.content.children[0].cloneNode(true);
@@ -440,6 +450,7 @@
             if (!ui.item) {
               e.target.value = "";
             }
+            $(e.target).autocomplete('destroy');
           }
         });
       }

resources/views/actor/show.blade.php
@@ -30,7 +30,7 @@
           <td><input type="text" class="noformat form-control" name="login" value="{{ $actorInfo->login }}"></td>
         </tr>
         <tr>
-          <td><label for="default_role" title="{{ $actorComments['default_role'] }}">Default role</label></td>
+          <td><label for="default_role" title="{{ $actorComments['default_role'] }}" title="Login needs to be null for changing the role">Default role</label></td>
           <td><input type="text" class="noformat form-control" name="default_role" data-ac="/role/autocomplete" value="{{ empty($actorInfo->droleInfo) ? '' : $actorInfo->droleInfo->name }}" {{ $actorInfo->login ? 'disabled' : 'autocomplete=off' }}></td>
         </tr>
         <tr>

resources/views/matter/classifiers.blade.php
@@ -45,14 +45,8 @@ $classifiers = $matter->classifiers->groupBy('type_name');
           <input type="hidden" name="matter_id" value="{{ $matter->container_id ?? $matter->id }}">
           <div class="form-row form-row-sm">
             <div class="col p-1">
-              <select name="type_code" class="custom-select custom-select-sm px-0">
-                <option value="">Select Type</option>
-                @foreach ( $classifierTypes as $cType )
-                <option value="{{ $cType->code }}">
-                  {{ $cType->type }}
-                </option>
-                @endforeach
-              </select>
+              <input type="hidden" name="type_code" value="">
+              <input type="text" class="form-control form-control-sm" size="16" placeholder="Type" data-ac="/classifier-type/autocomplete/0" data-actarget="type_code" data-aclength="0">
             </div>
             <div class="col p-1 hideForFile">
               <input type="text" class="form-control form-control-sm px-1" name="value" placeholder="Value">

resources/views/matter/create.blade.php
@@ -25,14 +25,8 @@
   <div class="form-group row">
     <label for="type_code" class="col-4 col-form-label">Type</label>
     <div class="col-8">
-      <select name="type_code" class="custom-select">
-        <option value=""></option>
-        @foreach ( $types as $type )
-        <option value="{{ $type->code }}" {{ $parent_matter->type_code == $type->code ? 'selected' : "" }}>
-          {{ $type->type }}
-        </option>
-        @endforeach
-      </select>
+      <input type="hidden" name="type_code" value="{{ $parent_matter->type_code ?? '' }}" />
+      <input type="text" class="form-control" data-ac="/type/autocomplete" data-actarget="type_code" data-aclength="0" placeholder="{{ $parent_matter->type->type ?? '' }}" autocomplete="off">
     </div>
   </div>
   <div class="form-group row">
@@ -68,6 +62,6 @@
   @endif
 
   <div>
-    <button type="button" id="createMatterSubmit" class="btn btn-primary">Create</button>
+    <button type="button" id="createMatterSubmit" class="btn btn-primary btn-block">Create</button>
   </div>
 </form>

resources/views/matter/edit.blade.php
@@ -1,16 +1,19 @@
+<div class="alert alert-info">Some fields cannot be changed once the matter is filed, because this would affect the deadline calculations</div>
 <form data-resource="/matter/{{ $matter->id }}">
-  <table class="table table-hover table-sm">
+  <table class="table">
     <tbody>
+      <tr>
+        <th width="25%">
+          Caseref
+        </th>
+        <td>
+          <input type="text" class="form-control noformat" name="caseref" value="{{ $matter->caseref }}">
+        </td>
+      </tr>
       <tr>
         <th>
           @if($cat_edit == 1)
-          <select name="category_code" class="noformat">
-            @foreach ( $categories as $cat )
-            <option value="{{ $cat->code }}" {{ $matter->category_code == $cat->code ? 'selected' : "" }}>
-              {{ $cat->category }}
-            </option>
-            @endforeach
-          </select>
+          <input type="text" class="form-control noformat" name="category_code" data-ac="/category/autocomplete" data-aclength="0" placeholder="{{ $matter->category->category }}">
           @else
           <span title="Cannot be changed because the matter has children">
             {{ $matter->category->category }}
@@ -19,13 +22,15 @@
         </th>
         <td>
           <div title="This value is changed automatically by inserting or deleting killer events, such as 'Abandoned', in the Status">
-            {{ $matter->dead == 1 ? 'Dead' : 'Alive' }}
+            {{ $matter->dead == 1 ? 'Inactive' : 'Active' }}
           </div>
         </td>
-        <td>Country</td>
+      </tr>
+      <tr>
+        <th>Country</th>
         <td>
           @if($country_edit == 1)
-          <input type="text" class="form-control noformat" name="country" data-ac="/country/autocomplete" placeholder="{{ $matter->countryInfo->name }}">
+          <input type="text" class="form-control noformat text-truncate" name="country" data-ac="/country/autocomplete" placeholder="{{ $matter->countryInfo->name }}">
           @else
           <span title="Cannot be changed because the matter has children">
             {{ $matter->countryInfo->name }}
@@ -34,51 +39,43 @@
         </td>
       </tr>
       <tr>
-        <td>Origin</td>
+        <th>Origin</th>
         <td>
-          <input type="text" class="form-control noformat" name="origin" data-ac="/country/autocomplete" value="{{ empty($matter->originInfo) ? '' : $matter->originInfo->name }}">
-        </td>
-        <td>Type</td>
-        <td>
-          <select name="type_code" class="noformat">
-            <option value=""></option>
-            @foreach ( $types as $type )
-            <option value="{{ $type->code }}" {{ $matter->type_code == $type->code ? 'selected' : "" }}>
-              {{ $type->type }}
-            </option>
-            @endforeach
-          </select>
+          <input type="text" class="form-control noformat text-truncate" name="origin" data-ac="/country/autocomplete" value="{{ empty($matter->originInfo) ? '' : $matter->originInfo->name }}">
         </td>
       </tr>
       <tr>
+        <th>Type</th>
         <td>
-          Caseref
+          <input type="text" class="form-control noformat" name="type_code" data-ac="/type/autocomplete" data-aclength="0" value="{{ $matter->type->type ?? '' }}">
         </td>
-        <td>
-          <input type="text" class="form-control noformat" name="caseref" value="{{ $matter->caseref }}">
-        </td>
-        <td>Responsible</td>
+      </tr>
+      <tr>
+        <th>Responsible</th>
         <td>
           <input type="text" class="form-control noformat" name="responsible" data-ac="/user/autocomplete" placeholder="{{ $matter->responsible }}">
         </td>
       </tr>
       <tr>
-        <td>Parent</td>
+        <th>Parent</th>
         <td>
           <input type="text" class="form-control noformat" name="parent_id" data-ac="/matter/autocomplete" value="{{ empty($matter->parent) ? '' : $matter->parent->uid }}">
         </td>
-        <td>Container
-        </td>
+      </tr>
+      <tr>
+        <th>Container</th>
         <td>
           <input type="text" class="form-control noformat" name="container_id" data-ac="/matter/autocomplete" value="{{ empty($matter->container) ? '' : $matter->container->uid }}">
         </td>
       </tr>
       <tr>
-        <td>Index</td>
+        <th>Index</th>
         <td>
           <input type="text" class="form-control noformat" id="idx" name="idx" value="{{ $matter->idx }}">
         </td>
-        <td>Term adjust</td>
+      </tr>
+      <tr>
+        <th>Term adjust (days)</th>
         <td>
           <input type="text" class="form-control noformat" id="term_adjust" name="term_adjust" value="{{ $matter->term_adjust }}">
         </td>

resources/views/matter/show.blade.php
@@ -63,14 +63,8 @@ $linkedBy = $matter->linkedBy->groupBy('type_code');
           <div class="form-row">
             <input type="hidden" name="matter_id" value="{{ $matter->container_id ?? $matter->id }}" />
             <div class="col-2">
-              <select name="type_code" class="custom-select custom-select-sm">
-                <option value=""></option>
-                @foreach ( $titleTypes as $tType )
-                <option value="{{ $tType->code }}">
-                  {{ $tType->type }}
-                </option>
-                @endforeach
-              </select>
+              <input type="hidden" name="type_code">
+              <input type="text" class="form-control form-control-sm" data-ac="/classifier-type/autocomplete/1" data-actarget="type_code" data-aclength="0" placeholder="Type" autocomplete="off">
             </div>
             <div class="col-10">
               <div class="input-group">

resources/views/user/create.blade.php
@@ -27,7 +27,7 @@
       <td>
 				<label for="default_role" title="{{ $userComments['default_role'] }}">Role *</label>
 				<input type="hidden" name="default_role">
-				<input type="text" class="form-control" data-ac="/dbrole/autocomplete" data-actarget="default_role">
+				<input type="text" class="form-control" data-ac="/dbrole/autocomplete" data-actarget="default_role" data-aclength="0">
 			</td>
       <td>
 				<label for="company_id" title="{{ $userComments['company_id'] }}">Company</label>

resources/views/user/show.blade.php
@@ -14,7 +14,7 @@
     </tr>
     <tr>
       <td><label for="default_role" title="{{ $userComments['default_role'] }}">Role</label></td>
-      <td><input type="text" class="noformat form-control" name="default_role" data-ac="/dbrole/autocomplete" value="{{ empty($userInfo->roleInfo) ? '' : $userInfo->roleInfo->name }}" autocomplete="off"></td>
+      <td><input type="text" class="noformat form-control" name="default_role" data-ac="/dbrole/autocomplete" data-aclength="0" value="{{ empty($userInfo->roleInfo) ? '' : $userInfo->roleInfo->name }}" autocomplete="off"></td>
     </tr>
     <tr>
       <td><label for="email">Email</label></td>

commit 521ad95ed964b36efae561fc2771660a79ac57f4

Fixed image file input toggling in matter.classifiers broken by commit 41197a2

resources/views/layouts/app.blade.php
@@ -142,7 +142,7 @@
   </div>
   <script>
     var contentSrc = "", // Identifies what to display in the Ajax-filled modal. Updated according to the href attribute used for triggering the modal
-      cTypeCode = '';
+      cTypeCode = ''; // Used for toggling image file input in matter.classifiers
 
     // Ajax fill an element from a url returning HTML
     var fetchInto = async (url, element) => {
@@ -373,14 +373,14 @@
         }
       }
       // matter.classifiers addClassifierForm - replace input fields with file upload field when selecting an image type
-      if (e.target.name === 'type_code' && e.target.value === 'IMG') {
+      if (e.target.dataset.actarget === 'type_code' && e.target.value === 'Image') {
         for (elt of addClassifierForm.getElementsByClassName('hideForFile')) {
           elt.classList.add('d-none');
         }
         forFile.classList.remove('d-none');
         cTypeCode = 'IMG'
       }
-      if (e.target.name === 'type_code' && e.target.value !== 'IMG' && cTypeCode === 'IMG') {
+      if (e.target.dataset.actarget === 'type_code' && e.target.value !== 'Image' && cTypeCode === 'IMG') {
         for (elt of addClassifierForm.getElementsByClassName('hideForFile')) {
           elt.classList.remove('d-none');
         }